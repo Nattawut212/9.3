afffd50a62da6f2ff7ec247485c7fe0a37364cd7
Nattawut212 <31862875+Nattawut212@users.noreply.github.com>
2018-10-31 11:09:46 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Nattawut</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="https://github.com/Nattawut212">Bom</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="https://nattawut224.000webhostapp.com/">00webhost</a></li>
+      <li class="active"><a href="http://localhost/menu02.php">Home</a></li>
+      <li><a href="abutme.php">Abutme</a></li>
+      <li class="dropdown">
+             <a class="dropdown-toggle" data-toggle="dropdown" href="#">64 Update
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="http://localhost/64%20update/categories/s0em.php">Allcategories</a></li>
+          <li><a href="http://localhost/64%20update/customers/s0em.php">Allcustomers</a></li>
+          <li><a href="http://localhost/64%20update/employees/s0em.php">Allemployees</a></li>          
+          <li><a href="http://localhost/64%20update/order_details/s0em.php">Allorder_details</a></li>	
+		  <li><a href="http://localhost/64%20update/products/s0em.php">Allproducts</a></li>
+		  <li><a href="http://localhost/64%20update/shippers/s0em.php">Allshippers</a></li>
+		  <li><a href="http://localhost/64%20update/suppliers/s0em.php">Allsuppliers</a></li>
+		
+
+        </ul>
+</li>
+
+<li class="dropdown">
+      <a class="dropdown-toggle" data-toggle="dropdown" href="#">8 Update
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="http://localhost/8db/categories.php">categoriesUpdate</a></li>
+          <li><a href="http://localhost/8db/customers.php">customersUpdate</a></li>
+		  <li><a href="http://localhost/8db/employee_update.php">employeeUpdate</a></li>
+		  <li><a href="http://localhost/8db/order_details.php">order_detailsUpdate</a></li>
+		  <li><a href="http://localhost/8db/orders.php">ordersUpdate</a></li>
+		  <li><a href="http://localhost/8db/products.php">productsUpdate</a></li>
+          <li><a href="http://localhost/8db/shippers.php">shippersUpdate</a></li>          
+          <li><a href="http://localhost/8db/suppliers.php">suppliersUpdate</a></li>		  
+        </ul>
+      </li>
+
+
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">36 peramit
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+		<li class="dropdown-submenu">
+		    <a class="more" href="#">Pyramid1-6<span class="caret"></span></a>
+		      <ul class="dropdown-menu">
+					<li><a href="bom_01.php">Pyramid1</a></li>
+					<li><a href="bom_02.php">Pyramid2</a></li> 
+					<li><a href="bom_03.php">Pyramid3</a></li>
+					<li><a href="bom_04.php">Pyramid4</a></li>
+					<li><a href="bom_05.php">Pyramid5</a></li>
+					<li><a href="bom_06.php">Pyramid6</a></li>
+				 </ul>
+			 </li>
+<li class="dropdown-submenu">
+		    <a class="more" href="#">Pyramid7-12<span class="caret"></span></a>
+		      <ul class="dropdown-menu">
+					<li><a href="bom_07.php">Pyramid7</a></li>
+					<li><a href="bom_08.php">Pyramid8</a></li>
+					<li><a href="bom_09.php">Pyramid9</a></li>
+					<li><a href="bom_10.php">Pyramid10</a></li>
+					<li><a href="bom11.php">Pyramid11</a></li>
+					<li><a href="bom12.php">Pyramid12</a></li>
+				 </ul>
+			 </li>
+
+			 <li class="dropdown-submenu">
+		    <a class="more" href="#">Pyramid13-18<span class="caret"></span></a>
+		      <ul class="dropdown-menu">
+					<li><a href="bom13.php">Pyramid13</a></li>
+					<li><a href="bom14.php">Pyramid14</a></li>
+					<li><a href="bom15.php">Pyramid15</a></li>
+					<li><a href="bom16.php">Pyramid16</a></li>
+					<li><a href="bom17.php">Pyramid17</a></li>
+					<li><a href="bom18.php">Pyramid18</a></li>
+				 </ul>
+			 </li>
+
+			 <li class="dropdown-submenu">
+		    <a class="more" href="#">Pyramid19-24<span class="caret"></span></a>
+		      <ul class="dropdown-menu">
+					<li><a href="bom19.php">Pyramid19</a></li>
+					<li><a href="bom20.php">Pyramid20</a></li>
+					<li><a href="bom21.php">Pyramid21</a></li>
+					<li><a href="bom22.php">Pyramid22</a></li>
+					<li><a href="bom23.php">Pyramid23</a></li>
+					<li><a href="bom24.php">Pyramid24</a></li>
+				 </ul>
+			 </li>
+
+			 <li class="dropdown-submenu">
+		    <a class="more" href="#">Pyramid25-30<span class="caret"></span></a>
+		      <ul class="dropdown-menu">
+					<li><a href="p_25.php">Pyramid25</a></li>
+					<li><a href="p_26.php">Pyramid26</a></li>
+					<li><a href="p_27.php">Pyramid27</a></li>
+					<li><a href="p_28.php">Pyramid28</a></li>
+					<li><a href="p_29.php">Pyramid29</a></li>
+					<li><a href="p_30.php">Pyramid30</a></li>
+				 </ul>
+			 </li>
+
+			 <li class="dropdown-submenu">
+		    <a class="more" href="#">Pyramid31-36<span class="caret"></span></a>
+		      <ul class="dropdown-menu">
+					<li><a href="p_31.php">Pyramid31</a></li>
+					<li><a href="p_32.php">Pyramid32</a></li>
+					<li><a href="p_33.php">Pyramid33</a></li>
+					<li><a href="p_35.php">Pyramid35</a></li>
+				 </ul>
+			 </li>
+        </ul>
+      </li>
+           
+        </ul>
+      </li>
+
+
+
+    </ul>	    
+    <ul class="nav navbar-nav navbar-right">
+    	<?php if(isset($_SESSION["user"]) && $_SESSION["user"] =="pass") { ?>
+      <li><a href="signin.php"><span class="glyphicon glyphicon-log-in"></span> signin</a></li>
+        <?php } ?>
+        <?php if(!isset($_SESSION["user"]) ||  $_SESSION["user"] !="pass") { ?>
+      <li><a href="logout.php"><span class="glyphicon glyphicon-off"></span> signout</a></li>
+     <?php } ?>
+    </ul>
+  </div> 
+</nav>
+<style>
+.dropdown-submenu {  position: relative; }
+.dropdown-submenu .dropdown-menu { top: 0; left: 100%; margin-top: -1px; }
+</style>
+<script>
+// $(document).ready(function(){
+var $j = jQuery.noConflict();
+$j(document).ready(function(){
+  $j('.dropdown-submenu a.more').on("click", function(e){
+    $j(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
+</script>
+<div class="container">
